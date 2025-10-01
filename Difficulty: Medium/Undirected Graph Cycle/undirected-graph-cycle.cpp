90f006c01a5ae8a659b5c316989d57a7395fd13a
Aksh2908 <151661402+Aksh2908@users.noreply.github.com>
2025-10-01 23:39:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,17 +1,15 @@
 class Solution {
   private:
-    bool dfs(vector<vector<int>>& adj, vector<bool>& visited, int src, int prev){
+    bool dfs(vector<vector<int>>& adj, vector<int>& visited, int src, int prev){
         visited[src]=1;
         for(auto it:adj[src]){
             if(!visited[it]){
                 if(dfs(adj,visited,it,src)) return true;
             }
-            else if(it!=prev){
-                return true;
-            }
+            else if(it!=prev) return true;
         }
         return false;
-    }  
+    }
   public:
     bool isCycle(int V, vector<vector<int>>& edges) {
         // Code here
@@ -24,11 +22,13 @@ class Solution {
             adj[v].push_back(u);
         }
         
-        vector<bool> visited(V,0);
+        vector<int> visited(V,0);
         
         for(int i=0;i<V;i++){
-            if(!visited[i]){    
-                if(dfs(adj,visited,i,-1)) return true;
+            if(!visited[i]){
+                if(dfs(adj,visited,i,-1)){
+                    return true;
+                }
             }
         }
         return false;
