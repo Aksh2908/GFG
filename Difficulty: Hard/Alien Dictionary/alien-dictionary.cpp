dfef23f054987cfeb3f83706da8aa114e896f368
Aksh2908 <151661402+Aksh2908@users.noreply.github.com>
2025-10-03 23:13:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Alien Dictionary/alien-dictionary.cpp
+++ Difficulty: Hard/Alien Dictionary/alien-dictionary.cpp
@@ -1,9 +1,11 @@
 class Solution {
   public:
     string findOrder(vector<string> &words) {
-        int n=words.size();
-        vector<vector<char>> graph(26);
+        // code here
+        int V=words.size();
+        vector<vector<int>> graph(26,vector<int>());
         vector<int> present(26,0);
+        
         
         for(auto it:words){
             for(int i=0;i<it.length();i++){
@@ -11,63 +13,68 @@ class Solution {
             }
         }
         
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<V-1;i++){
             string s1=words[i];
             string s2=words[i+1];
             int j=0,k=0;
             
             while(j<s1.length() && k<s2.length()){
                 if(s1[j]==s2[k]){
-                    j++;
-                    k++;
+                    j++;k++;
                 }
                 else{
-                    graph[s1[j]-'a'].push_back(s2[k]);
+                    graph[s1[j]-'a'].push_back(s2[k]-'a');
+                    //cout << s1[j] << " " << s2[k] << endl;
                     break;
                 }
             }
-            
-            if (j == s1.length() || k == s2.length()) {
-                if (s1.length() > s2.length()) return "";
-            }
-            
-        }
-        
-        int v=graph.size();
-        vector<int> indeg(26,0);
-        vector<int> visited(26,0);
-        queue<char> q;
-        
-        for(int i=0;i<26;i++){
-            for(char x:graph[i]){
-                indeg[x-'a']++;
+            if (j == s2.length() && s1.length() > s2.length()){
+                return "";
             }
         }
         
-        for(int i=0;i<26;i++){
-            if(!indeg[i] && present[i]){
-                q.push(i+'a');
-                visited[i]=1;
+        // for(int i=0;i<26;i++){
+        //     for(auto it:graph[i]){
+        //         cout << i << "->" << it << " ";
+        //     }
+        //     cout << endl;
+        // }
+        
+        vector<int> indeg(graph.size(),0);
+        for(int i=0;i<graph.size();i++){
+            for(auto it:graph[i]){
+                indeg[it]++;
             }
         }
+        // cout << endl;
+        // for(auto it:indeg){
+        //     cout << it << " ";
+        // }
         
-        string ans="";
+        queue<int> q;
+        
+        for(int i=0;i<indeg.size();i++){
+            if(!indeg[i] && present[i]==1) q.push(i);
+        }
+        
+        string st="";
         
         while(!q.empty()){
-            char node=q.front();
-            ans+=node;
+            int node=q.front();
+            st+=(node+'a');
             q.pop();
             
-            for(auto it:graph[node-'a']){
-                indeg[it-'a']--;
-                if(!indeg[it-'a']){
-                    q.push(it);
-                    visited[it-'a']=1;
-                }
+            for(auto it:graph[node]){
+                indeg[it]--;
+                if(!indeg[it] && present[it]) q.push(it);
             }
         }
+        
+        //cout << st << " ";
+        
         int cnt=accumulate(present.begin(),present.end(),0);
-        if(ans.size()!=cnt) return "";
-        return ans;
+        if(cnt!=st.length()) return "";
+        
+        return st;
     }
 };
