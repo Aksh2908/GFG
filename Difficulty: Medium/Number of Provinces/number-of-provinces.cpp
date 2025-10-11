176236f25806a18049ebe76e87e092eebf21f6fc
Aksh2908 <151661402+Aksh2908@users.noreply.github.com>
2025-10-11 15:12:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Provinces/number-of-provinces.cpp
+++ Difficulty: Medium/Number of Provinces/number-of-provinces.cpp
@@ -1,73 +1,55 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-//User function Template for C++
-
-class Solution {
-  private:
-    void dfs(int src, vector<int>& visited, vector<vector<int>>& adj){
-        visited[src]=1;
-        for(auto it:adj[src]){
-            if(!visited[it]) dfs(it,visited,adj);
+// User function Template for C++
+class disjoint{
+    public:
+        vector<int> parent,size;
+        disjoint(int n){
+            parent.resize(n+1);
+            size.resize(n+1,1);
+            for(int i=0;i<n;i++){
+                parent[i]=i;
+            }
         }
-    }
+        
+        int findParent(int u){
+            if(u==parent[u]) return u;
+            return parent[u]=findParent(parent[u]);
+        }
+        
+        void unionBySize(int u, int v){
+            int pu=findParent(u);
+            int pv=findParent(v);
+            if(size[pu]<size[pv]){
+                parent[pu]=pv;
+                size[pv]+=size[pu];
+            }
+            else{
+                parent[pv]=pu;
+                size[pu]+=size[pv];
+            }
+        }
+};
+class Solution {
   public:
     int numProvinces(vector<vector<int>> adj, int V) {
+        // code here
         vector<vector<int>> adjL(V);
         int i,j;
+        disjoint ds(V);
+        
         for(i=0;i<V;i++){
             for(j=0;j<V;j++){
                 if(adj[i][j] && i!=j){
-                    adjL[i].push_back(j);
-                    adjL[j].push_back(i);
+                    ds.unionBySize(i,j);
                 }
             }
         }
+        
+        
         int cnt=0;
-        vector<int> visited(V);
-        for(i=0;i<V;i++){
-            if(!visited[i]){
-                cnt++;
-                dfs(i,visited,adjL);
-            }
+        
+        for(int i=0;i<V;i++){
+            if(ds.findParent(i)==i) cnt++;
         }
         return cnt;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int V,x;
-        cin>>V;
-        
-        vector<vector<int>> adj;
-        
-        for(int i=0; i<V; i++)
-        {
-            vector<int> temp;
-            for(int j=0; j<V; j++)
-            {
-                cin>>x;
-                temp.push_back(x);
-            }
-            adj.push_back(temp);
-        }
-        
-
-        Solution ob;
-        cout << ob.numProvinces(adj,V) << endl;
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-// } Driver Code Ends
