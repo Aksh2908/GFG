ef53d169a04457ace33caec329b1d0e7bb0b5be1
Aksh2908 <151661402+Aksh2908@users.noreply.github.com>
2025-11-15 23:37:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
@@ -1,11 +1,9 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
+/*
+class Node {
+public:
     int data;
-    struct Node* next;
-    struct Node* bottom;
+    Node* next;
+    Node* bottom;
 
     Node(int x) {
         data = x;
@@ -13,117 +11,39 @@ struct Node {
         bottom = NULL;
     }
 };
-
-void printList(Node* head) {
-    while (head != NULL) {
-        printf("%d ", head->data);
-        head = head->bottom;
-    }
-    printf("\n");
-}
-
-Node* createLinkedList(vector<Node*>& v) {
-    Node* head = new Node(0);
-    Node* temp = head;
-    int n = v.size();
-    for (int i = 0; i < n; i++) {
-        temp->next = v[i];
-        temp = temp->next;
-    }
-    return head->next;
-}
-
-
-// } Driver Code Ends
-
-/* Node structure  used in the program
-
-struct Node{
-    int data;
-    struct Node * next;
-    struct Node * bottom;
-
-    Node(int x){
-        data = x;
-        next = NULL;
-        bottom = NULL;
-    }
-
-};
 */
 
 class Solution {
   private:
-    Node* merge(Node* l1, Node* l2){
-        Node* dummy = new Node(-1);
-        Node* sorted = dummy;
+    Node *merged(Node* l1, Node* l2){
+        if(!l1) return l2;
+        if(!l2) return l1;
+        
+        Node* dummy=new Node(-1);
+        Node* temp=dummy;
+        
         while(l1 && l2){
             if(l1->data<=l2->data){
-                sorted->bottom=l1;
+                temp->bottom=l1;
                 l1=l1->bottom;
-            }
+            }    
             else{
-                sorted->bottom=l2;
+                temp->bottom=l2;
                 l2=l2->bottom;
             }
-            sorted=sorted->bottom;
+            temp=temp->bottom;
         }
-        if(l1){
-            sorted->bottom=l1;
-        }
-        else{
-            sorted->bottom=l2;
-        }
+        
+        if(l1) temp->bottom=l1;
+        if(l2) temp->bottom=l2;
+        
         return dummy->bottom;
     }
   public:
-    // Function which returns the  root of the flattened linked list.
     Node *flatten(Node *root) {
+        // code here
         if(!root || !root->next) return root;
-        Node* mergeHead=flatten(root->next);
-        return merge(root,mergeHead);
+        Node* mergedHead=flatten(root->next);
+        return merged(mergedHead,root);
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int n;
-        cin >> n;
-        cin.ignore();
-
-        vector<Node*> v(n);
-
-        for (int i = 0; i < n; i++) {
-            string line;
-            getline(cin, line);
-            stringstream ss(line);
-
-            Node* head = new Node(0);
-            Node* temp = head;
-            int x;
-            while (ss >> x) {
-                Node* newNode = new Node(x);
-                temp->bottom = newNode;
-                temp = temp->bottom;
-            }
-            v[i] = head->bottom;
-        }
-
-        Solution ob;
-        Node* list = createLinkedList(v);
-        Node* head = ob.flatten(list);
-        printList(head);
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
