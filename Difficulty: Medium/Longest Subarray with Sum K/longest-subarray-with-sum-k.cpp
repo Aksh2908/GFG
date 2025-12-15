7f8c845c78031f3c89e8fb64670fd64145f4958f
Aksh2908 <151661402+Aksh2908@users.noreply.github.com>
2025-12-15 22:37:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.cpp
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.cpp
@@ -1,65 +1,32 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     int longestSubarray(vector<int>& arr, int k) {
-        unordered_map<long long,int> mpp;
-        int n=arr.size(),i;
-        long long sum=0;
+        // code here
+        int n=arr.size();
+        
         int maxLen=0;
+        long long sum=0;
+        
         if(!n) return 0;
+        
+        unordered_map<long long,int> mpp;
         mpp[0]=-1;
-        for(i=0;i<n;i++){
+        
+        for(int i=0;i<n;i++){
             sum+=arr[i];
             if(sum==k){
                 maxLen=max(maxLen,i+1);
             }
-            long long diff=sum-k;
-            if(mpp.find(diff)!=mpp.end()){
-                maxLen=max(maxLen,i-mpp[diff]);
+            
+            long long rem=sum-k;
+            
+            if(mpp.find(rem)!=mpp.end()){
+                maxLen=max(maxLen,i-mpp[rem]);  
             }
-            if (mpp.find(sum) == mpp.end()) {
-                mpp[sum] = i;
+            if(mpp.find(sum)==mpp.end()){
+                mpp[sum]=i;
             }
         }
         return (int)maxLen;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // Ignore newline character after t
-
-    while (t--) {
-        vector<int> arr;
-        int k;
-        string inputLine;
-
-        getline(cin, inputLine); // Read the array input as a line
-        stringstream ss(inputLine);
-        int value;
-        while (ss >> value) {
-            arr.push_back(value);
-        }
-
-        cin >> k;
-        cin.ignore(); // Ignore newline character after k input
-
-        Solution solution;
-        cout << solution.longestSubarray(arr, k) << "\n";
-        cout << "~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
