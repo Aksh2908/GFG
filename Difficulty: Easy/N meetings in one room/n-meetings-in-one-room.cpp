5cf26ce7090cb668de8b0bf8af99d284d7344383
Aksh2908 <151661402+Aksh2908@users.noreply.github.com>
2026-01-27 23:14:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
@@ -1,74 +1,29 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     // Function to find the maximum number of meetings that can
     // be performed in a meeting room.
-    static bool cmp(pair<int,int> a, pair<int,int> b){
-        return a.second < b.second;
-    }
-    
     int maxMeetings(vector<int>& start, vector<int>& end) {
-        vector<pair<int,int>> ans;
-        int n=end.size();
+        // Your code here
+        int n=start.size();
+        vector<pair<int,int>> meet(n);
         
         for(int i=0;i<n;i++){
-            ans.push_back({start[i],end[i]});
+            meet[i]={end[i],start[i]};
         }
         
-        sort(ans.begin(),ans.end(),cmp);
+        sort(meet.begin(),meet.end());
         
-        int endTime=ans[0].second;
+        int startTime=-1;
+        int endTime=-1;
+        int cnt=0;
         
-        int cnt=1;
-        
-        for(int i=1;i<n;i++){
-            if(ans[i].first>endTime){
+        for(int i=0;i<n;i++){
+            if(meet[i].first>endTime && meet[i].second>startTime){
                 cnt++;
-                endTime=ans[i].second;
+                startTime=meet[i].first;
+                endTime=meet[i].second;
             }
         }
-        
         return cnt;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        string input;
-        int num;
-        vector<int> start;
-
-        getline(cin, input);
-        stringstream s2(input);
-        while (s2 >> num) {
-            start.push_back(num);
-        }
-
-        vector<int> end;
-        getline(cin, input);
-        stringstream s22(input);
-        while (s22 >> num) {
-            end.push_back(num);
-        }
-
-        Solution ob;
-        int ans = ob.maxMeetings(start, end);
-        cout << ans << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
