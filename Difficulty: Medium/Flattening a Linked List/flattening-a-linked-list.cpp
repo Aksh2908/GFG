ec9b474910a85d3c68bb737b2e95cdb069a9b451
Aksh2908 <151661402+Aksh2908@users.noreply.github.com>
2026-04-11 00:12:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
@@ -15,27 +15,27 @@ public:
 
 class Solution {
   private:
-    Node *merged(Node* l1, Node* l2){
-        if(!l1) return l2;
-        if(!l2) return l1;
-        
-        Node* dummy=new Node(-1);
+    Node* merge(Node* l1, Node* l2){
+        Node* dummy= new Node(-1);
         Node* temp=dummy;
         
         while(l1 && l2){
             if(l1->data<=l2->data){
                 temp->bottom=l1;
                 l1=l1->bottom;
-            }    
+            }
             else{
                 temp->bottom=l2;
                 l2=l2->bottom;
             }
             temp=temp->bottom;
         }
-        
-        if(l1) temp->bottom=l1;
-        if(l2) temp->bottom=l2;
+        if(l1){
+            temp->bottom=l1;
+        }
+        if(l2){
+            temp->bottom=l2;
+        }
         
         return dummy->bottom;
     }
@@ -43,7 +43,7 @@ class Solution {
     Node *flatten(Node *root) {
         // code here
         if(!root || !root->next) return root;
-        Node* mergedHead=flatten(root->next);
-        return merged(mergedHead,root);
+        Node* flattenHead=flatten(root->next);
+        return merge(flattenHead,root);
     }
 };
