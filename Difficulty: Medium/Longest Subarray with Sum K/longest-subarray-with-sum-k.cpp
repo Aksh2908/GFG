06450103bc14bd18ab91d8a4e880edda045c3832
Aksh2908 <151661402+Aksh2908@users.noreply.github.com>
2026-04-12 15:11:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.cpp
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.cpp
@@ -3,29 +3,26 @@ class Solution {
     int longestSubarray(vector<int>& arr, int k) {
         // code here
         int n=arr.size();
-        
         int maxLen=0;
-        long long sum=0;
-        if(!n) return 0;
+        
         unordered_map<long long,int> mpp;
+        
         mpp[0]=-1;
+        
+        long long sum=0;
         
         for(int i=0;i<n;i++){
             sum+=arr[i];
-            if(sum==k){
-                maxLen=max(maxLen,i+1);
+            
+            int rem=sum-k;
+            
+            if(mpp.find(rem)!=mpp.end()){
+                maxLen=max(maxLen,i-mpp[rem]);
             }
-            
-            int diff=sum-k;
-            
-            if(mpp.find(diff)!=mpp.end()){
-                maxLen=max(maxLen,i-mpp[diff]);
-            }
-            
-            if(mpp.find(sum)==mpp.end()){
+            if(mpp.find(sum)==mpp.end())
                 mpp[sum]=i;
-            }
-        }
-        return (int)maxLen;
+        }   
+        
+        return maxLen;
     }
 };
