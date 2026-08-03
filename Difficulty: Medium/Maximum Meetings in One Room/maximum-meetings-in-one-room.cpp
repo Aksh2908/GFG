84a2c87df9fff09b2274e80dce24f3e5492245c7
Aksh2908 <151661402+Aksh2908@users.noreply.github.com>
2026-08-03 23:30:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Meetings in One Room/maximum-meetings-in-one-room.cpp
+++ Difficulty: Medium/Maximum Meetings in One Room/maximum-meetings-in-one-room.cpp
@@ -1,75 +1,30 @@
-//{ Driver Code Starts
-//Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function Template for C++
-
 class Solution {
   public:
-    static bool cmp(pair<int,pair<int,int>> a, pair<int,pair<int,int>> b){
-        return a.first < b.first;
-    }
-    
-    vector<int> maxMeetings(int N, vector<int> &S, vector<int> &F) {
-        vector<pair<int,pair<int,int>>> meetings;
-        int n=F.size();
+    vector<int> maxMeetings(vector<int> &s, vector<int> &f) {
+        // code here
+        int n=s.size();
+        
+        vector<pair<int,pair<int,int>>> ds;
         
         for(int i=0;i<n;i++){
-            meetings.push_back({F[i],{S[i],i+1}});
+            ds.push_back({f[i],{i+1,s[i]}});
         }
         
-        sort(meetings.begin(),meetings.end(),cmp);
+        sort(ds.begin(),ds.end());
         
-        int endTime=meetings[0].first;
+        int endtime=-1;
         
         vector<int> ans;
-        ans.push_back(meetings[0].second.second);
         
-        for(int i=1;i<n;i++){
-            if(meetings[i].second.first>endTime){
-                ans.push_back(meetings[i].second.second);
-                endTime=meetings[i].first;
+        for(int i=0;i<n;i++){
+            if(endtime<ds[i].second.second){
+                ans.push_back(ds[i].second.first);
+                endtime=ds[i].first;
             }
         }
         
         sort(ans.begin(),ans.end());
+        
         return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        vector<int> S(n),F(n);
-        for(int i=0;i<n;i++){
-            cin>>S[i];
-        }
-        for(int i=0;i<n;i++){
-            cin>>F[i];
-        }
-        Solution ob;
-        vector<int> ans=ob.maxMeetings(n,S,F);
-        for(int i=0;i<ans.size();i++){
-            cout<<ans[i];
-            if(i!=ans.size()-1){
-                cout<<" ";
-            }
-        }
-        cout<<endl;
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-// } Driver Code Ends
