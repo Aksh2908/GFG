02dcde2d4e0136ea413a65ce3405080b60b0de47
Aksh2908 <151661402+Aksh2908@users.noreply.github.com>
2026-08-11 06:28:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Job first/shortest-job-first.cpp
+++ Difficulty: Medium/Shortest Job first/shortest-job-first.cpp
@@ -1,50 +1,19 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function Template for C++
-
-//Back-end complete function Template for C++
-
 class Solution {
   public:
-    long long solve(vector<int>& bt) {
-        sort(bt.begin(),bt.end());
-        int i,waitingTime=0;
+    int solve(vector<int>& bt) {
+        // code here
         int n=bt.size();
-        int sum=0;
-        for(i=0;i<n;i++){
-            waitingTime+=sum;
-            sum+=bt[i];
+        
+        sort(bt.begin(),bt.end());
+        
+        int st=0;
+        int twt=0;
+        
+        for(int i=0;i<n;i++){
+            twt+=(st);
+            st+=(bt[i]);
         }
-        return floor((double)waitingTime/n);
+        
+        return twt/n;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<int> bt(n), p(n);
-        for (int i = 0; i < n; i++) {
-            cin >> bt[i];
-        }
-        Solution obj;
-        long long ans = obj.solve(bt);
-        cout << ans << "\n";
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-// } Driver Code Ends
